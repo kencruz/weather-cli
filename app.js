8f8365eef6b55ad3e910f3b88996bf3d21426890
Ken Cruz <ken@kencruz.ca>
2021-04-01 10:23:27 -0400

    implemented initial temperature flag options

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 require("dotenv").config();
 
-console.log("hello world!");
+const yargs = require("yargs");
+
+const argv = yargs
+  .option("farhenheit", {
+    alias: "f",
+    description: "Show the number of degrees in farhenheit",
+    type: "number",
+  })
+  .option("celsius", {
+    alias: "c",
+    description: "Show the number of degrees in celsius",
+    type: "number",
+  })
+  .help()
+  .alias("help", "h").argv;
+
+if (!argv.farhenheit && !argv.celsius) {
+  console.log(
+    "ERROR: Must have at least --farhenheit -f or --celsius -c flags in arguments!"
+  );
+}
