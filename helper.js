f67caffc55ca6f9622e40ca2e190b7ddde5bdefc
Ken Cruz <ken@kencruz.ca>
2021-06-04 21:26:29 -0400

    implemented color for temperature values

--- helper.js
+++ helper.js
@@ -6,11 +6,35 @@ function toCelsius(kelvin) {
   return (kelvin - 273.15).toFixed(2);
 }
 
+function tempColor(kelvin, tempString) {
+  if (kelvin > 303) {
+    return tempString.toString().red;
+  }
+  if (kelvin > 293) {
+    return tempString.toString().yellow;
+  }
+  if (kelvin > 283) {
+    return tempString.toString().gray;
+  }
+  if (kelvin > 273) {
+    return tempString.toString().cyan;
+  }
+  return tempString.toString().blue;
+}
+
 exports.temperatureString = (celsius, farhenheit, kelvin) => {
-  if (celsius && farhenheit)
-    return `${toCelsius(kelvin)}C / ${toFahrenheit(kelvin)}F`;
+  if (celsius && farhenheit) {
+    const tempString = `${toCelsius(kelvin)}C / ${toFahrenheit(kelvin)}F`;
+    return tempColor(kelvin, tempString);
+  }
 
-  if (celsius) return `${toCelsius(kelvin)}C`;
+  if (celsius) {
+    const tempString = `${toCelsius(kelvin)}C`;
+    return tempColor(kelvin, tempString);
+  }
 
-  if (farhenheit) return `${toFahrenheit(kelvin)}F`;
+  if (farhenheit) {
+    const tempString = `${toFahrenheit(kelvin)}F`;
+    return tempColor(kelvin, tempString);
+  }
 };
