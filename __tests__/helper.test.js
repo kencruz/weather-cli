d99b7bc88440697358a3704cf35b0eb2def427e9
Ken Cruz <ken@kencruz.ca>
2021-06-17 22:37:44 -0400

    added test for kelvin to farhenheit

--- __tests__/helper.test.js
+++ __tests__/helper.test.js
@@ -1,5 +1,9 @@
 const { temperatureString } = require("../helper");
 
-test("temp string", () => {
+test("temp string in celsius", () => {
   expect(temperatureString(true, false, 289).temperature).toBe("15.85C");
+});
+
+test("temp string in farhenheit", () => {
+  expect(temperatureString(false, false, 289).temperature).toBe("60.53F");
 });
