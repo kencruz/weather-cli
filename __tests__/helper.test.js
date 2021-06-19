73c300d0e2f2d95d4dd8df3297bc94d3b36f1419
Ken Cruz <ken@kencruz.ca>
2021-06-18 23:27:02 -0400

    added test for kelvin to farhenheit & celsius

--- __tests__/helper.test.js
+++ __tests__/helper.test.js
@@ -7,3 +7,9 @@ test("temp string in celsius", () => {
 test("temp string in farhenheit", () => {
   expect(temperatureString(false, false, 289).temperature).toBe("60.53F");
 });
+
+test("temp string in celsius, farhenheit", () => {
+  expect(temperatureString(true, true, 289).temperature).toBe(
+    "15.85C / 60.53F"
+  );
+});
